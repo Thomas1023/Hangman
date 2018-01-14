df559e08f033a7aca2212848c7f43413c5f16521
NikitaD111 <league@WorthyMini-6.attlocal.net>
2018-01-14 12:58:34 -0800

    do you know da whey
    yes i know da whey

--- src/HangBro/HangPane.java
+++ src/HangBro/HangPane.java
@@ -1,4 +1,5 @@
 package HangBro;
+
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -13,58 +14,60 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class HangPane implements KeyListener{
-	 static ArrayList<String> ze = new ArrayList<String>();
-	 static String LE;
-	 static int Lives =10;
-	 static String L="";
-	 
- public  void HangMaker() {
-	 
-	 JFrame JF = new JFrame();
-	 JPanel JP = new JPanel();
-	 JLabel JL = new JLabel();
-	 JLabel man = new JLabel("웃");
-	 JLabel Life = new JLabel();
-	 JF.add(JP);
-	 JP.add(man);
-	 JP.add(Life);
-	JF.setSize(500, 300);
-	 JF.setVisible(true);
-	 int h=2999;
-	 JP.add(man);
-	 JF.setTitle("HangMan");
-	 JP.add(JL);
-	
-	 HangRead();
-	man.setFont(new Font(man.getFont().getName(), man.getFont().getStyle(),100));
-	Life.setFont(new Font(man.getFont().getName(), man.getFont().getStyle(),100));
-	 Random rand = new Random(); 
-	 int value = rand.nextInt(2999);
-	  LE = ze.get(value);
-	 System.out.println(ze.get(value));
-	 ze.remove(value);
-	 h--;
-	 for(int e1=0; e1<LE.length();e1++) {
-				L=L+"_ ";	
+public class HangPane implements KeyListener {
+	static ArrayList<Character> used = new ArrayList<Character>();
+	static ArrayList<String> ze = new ArrayList<String>();
+	static String LE;
+	static int Lives = 10;
+	static String L = "";
+	static String tempstr="";
+		JFrame JF = new JFrame();
+		JPanel JP = new JPanel();
+		JLabel JL = new JLabel();
+		JLabel man = new JLabel("웃");
+		JLabel Life = new JLabel();
+	public void HangMaker() {
+
+		JF.add(JP);
+		JP.add(man);
+		JP.add(Life);
+		JF.setSize(500, 300);
+		JF.setVisible(true);
+		int h = 2999;
+		JP.add(man);
+		JF.setTitle("HangMan");
+		JP.add(JL);
+
+		HangRead();
+		man.setFont(new Font(man.getFont().getName(), man.getFont().getStyle(), 100));
+		Life.setFont(new Font(man.getFont().getName(), man.getFont().getStyle(), 100));
+		Random rand = new Random();
+		int value = rand.nextInt(2999);
+		LE = ze.get(value);
+		System.out.println(ze.get(value));
+		ze.remove(value);
+		h--;
+		for (int e1 = 0; e1 < LE.length(); e1++) {
+			L = L + "_";
+		}
+		JL.setText(L);
+		JF.addKeyListener(this);
+		// JP.requestFocus();
+		Life.setText("" + Lives);
 	}
-	 JL.setText(L);
-	 JF.addKeyListener(this);
-	//JP.requestFocus();
-	 Life.setText(""+Lives);
- }
- public  void HangRead() {
-	
-	 try {
+
+	public void HangRead() {
+
+		try {
 			BufferedReader br = new BufferedReader(new FileReader("src/HangBro/dictionary.txt"));
-			
+
 			String line = br.readLine();
-			while(line != null){
+			while (line != null) {
 				ze.add(line);
 				System.out.println(line);
 				line = br.readLine();
 			}
-			
+
 			br.close();
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
@@ -73,47 +76,73 @@ public class HangPane implements KeyListener{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- }
-@Override
-public void keyTyped(KeyEvent e) {
+	}
 
-	boolean gotit = false;
-	 char ch = e.getKeyChar();
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+		boolean gotit = false;
+		char ch = e.getKeyChar();
 		System.out.println(ch);
-		
-		for(int i=0; i<LE.length();i++) {
-		if(LE.charAt(i) == ch) {
-			gotit=true;
-			
-				for(int e1=0; e1<LE.length();e1++) {
-				if(e1==i) {
-					L=L+ch+" ";
-					
-				}else {
-					 
+
+		for (int i = 0; i < LE.length(); i++) {
+			if (LE.charAt(i) == ch) {
+				gotit = true;
+
+				for (int e1 = 0; e1 < LE.length(); e1++) {
+					if (e1 == i) {
+						L = L + ch + " ";
+
+					} else {
+
+					}
+
 				}
-		
-		 }
-		}else{
-			
+			} else {
+
+			}
+
 		}
-		
-		}
-	if(gotit) {	
-		System.out.println("Corret");
-		}else {
+		if (gotit) {
+
+			System.out.println("Corret");
+			tempstr="";
+			for (int e1 = 0; e1 < LE.length(); e1++) {
+				
+				System.err.println(ch+"="+LE.charAt(e1));
+				
+				if (LE.charAt(e1) == ch) {
+					tempstr = tempstr + LE.charAt(e1);
+					used.add(LE.charAt(e1));
+				} else if(used.contains(LE.charAt(e1))) {
+					tempstr = tempstr + LE.charAt(e1);
+				}
+				else {
+					tempstr = tempstr + "_";
+					System.out.println();
+				}
+				
+				
+				
+				
+			}
+			L = tempstr;
+			System.out.println(L);
+			JL.setText(L);
+		} else {
 			Lives--;
 			System.err.println("Wrong");
-			
-		}
-}
-@Override
-public void keyPressed(KeyEvent e) {
-	
-}
-@Override
-public void keyReleased(KeyEvent e) {
 
-	
-}
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
 }
